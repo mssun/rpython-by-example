d85483a92ef3976358266c3c6950d92fa000d99f
Mingshen Sun <mssun@mesalock-linux.org>
2018-12-10 15:31:23 -0800

    code/hello_world: add comments to the sources

--- code/hello_world.py
+++ code/hello_world.py
@@ -1,6 +1,8 @@
-def entrypoint(args):
+def entry_point(argv):
     print "Hello, World!"
     return 0
 
+# The target function is the main function of a RPython program. It takes
+# command line arguments as inputs and return an entry point function.
 def target(*args):
-    return entrypoint, None
+    return entry_point
