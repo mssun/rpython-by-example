9046e9db87fc3506ec418ebdde1b3ac5dd27ae28
Mingshen Sun <mssun@mesalock-linux.org>
2019-03-29 11:12:20 -0700

    code/builtin: add more comments

--- code/builtin.py
+++ code/builtin.py
@@ -1,23 +1,23 @@
 def builtin_functions():
-    print bool(1)
-    print int(1.0)
-    print float(1)
-    print chr(65)
-    print str(53)
-    print unichr(97)
-    print unicode('abc')
-    print bytearray('abc')
-    print list('abc')
-    for i in range(3):
+    print bool(1)     # return a boolean value
+    print int(1.0)    # return an integer value
+    print float(1)    # return a float value
+    print chr(65)     # return a string of one character whose ASCII code is the input
+    print str(53)     # return a string containing a nicely printable representation of an object
+    print unichr(97)  # return the Unicode string of one character whose Unicode code is the integer input
+    print unicode('abc')    # return the Unicode string version of object
+    print bytearray('abc')  # return a new array of bytes
+    print list('abc')       # return a list whose items are the same and in the same order as iterable’s items
+    for i in range(3):      # create a list containing arithmetic progressions
         print i
     for i in xrange(3):
         print i
     seasons = ['Spring', 'Summer', 'Fall', 'Winter']
-    for i in enumerate(seasons):
+    for i in enumerate(seasons):    # return an enumerate object
         print i
-    print min(1, 2)
-    print max(1, 2)
-    for i in reversed([1, 2, 3]):
+    print min(1, 2)    # return the smallest of two arguments
+    print max(1, 2)    # return the largest of two arguments
+    for i in reversed([1, 2, 3]):    # return a reverse iterator
         print i
 
 def entry_point(argv):
